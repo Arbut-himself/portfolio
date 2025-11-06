1e182485f9bdec3a333ed25a79a868b6a187082b
Arbut-himself <prebelief@gmail.com>
2025-11-06 23:20:59 +0200

    Refactored the code. Added two separate functions for repeat behaviour in main function

--- python/freecodecamp_daily_coding_challenges/2025_november/fcc_dcc_2025-11-01.py
+++ python/freecodecamp_daily_coding_challenges/2025_november/fcc_dcc_2025-11-01.py
@@ -1,22 +1,24 @@
-def verify(message: str, key: str, signature: int) -> bool:
+def alphabetize(input: str) -> str:
+    output = ''.join(filter(str.isalpha, input))
+    return output
 
+def count_letter_values(input: str) -> int:
     count = 0
-    
-    alpha_only_message = ''.join(filter(str.isalpha, message))
-    alpha_only_key = ''.join(filter(str.isalpha, key))
-
-    for character in alpha_only_message:
+    for character in input:
         if character.isupper():
             count += (ord(character)-38)
         else:
             count += (ord(character)-96)
+    return count
+        
+def verify(message: str, key: str, signature: int) -> bool:
+    count = 0
 
-    for character in alpha_only_key:
-        if character.isupper():
-            count += (ord(character)-38)
-        else:
-            count += (ord(character)-96)
+    alpha_only_message = alphabetize(message)
+    alpha_only_key = alphabetize(key)
+
+    count += count_letter_values(alpha_only_message)
+
+    count += count_letter_values(alpha_only_key)
 
     return count == signature
-
-print(verify("foo", "bar", 57))
